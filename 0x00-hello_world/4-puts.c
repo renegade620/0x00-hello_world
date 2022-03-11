0cd64405e95c73ad717fcf093ebca920341653cd
Omondi Were <franklinegift@gmail.com>
2022-03-10 18:57:02 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints displayed text
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
