98bd40257b922311d1456d5fb84c8307b9516887
Omondi Were <franklinegift@gmail.com>
2022-03-10 18:28:39 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main displays given text
+ * return returns zero since main(void)
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
