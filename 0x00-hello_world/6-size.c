a82b6c4bc43c7be8416a19af4c701d6501fe1a7f
Omondi Were <franklinegift@gmail.com>
2022-03-10 18:44:44 -0800

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints sizeof variable types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+	int y;
+	long int z;
+	long long a;
+	float b;
+
+	printf("Size of a char: %d byte(s) \n", sizeof(x));
+	printf("Size of an int: %d byte(s)\n", sizeof(y));
+	printf("Size of a long int: %d byte(s)\n", sizeof(z));
+	printf("Size of a long long int: %d byte(s)\n" sizeof(a));
+	printf("Size of a float: %d byte(s)\n" sizeof(b));
+	return(0);
+}
