84dc828e2ebc61baa6928023e102c963adb876dd
Omondi Were <franklinegift@gmail.com>
2022-03-11 17:51:57 -0800

    for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph[26] = "abcdefghiklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	       putchar(alph[i]);
+        putchar("\n");
+        return (0);
+
+}
