a8231c793aa84d8ba230a1fceb90774ad63ab725
Omondi Were <franklinegift@gmail.com>
2022-03-13 15:48:23 -0700

    Advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print text as it is
+ *
+ * Return: 1 (error)
+ */
+int main()
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
