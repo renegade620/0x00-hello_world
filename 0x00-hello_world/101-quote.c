9937346de11727cc4e23efbc2a5870f230d3b510
Omondi Were <franklinegift@gmail.com>
2022-03-13 16:10:39 -0700

    Advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - print text as it is
  *
  * Return: 1 (error)
  */
-int main()
+int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
