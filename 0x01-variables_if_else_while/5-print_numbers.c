8bf7e795c40ba3da172a5bb40b7421a54c813295
Omondi Were <franklinegift@gmail.com>
2022-03-14 07:54:53 -0700

    print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 0; x <= 9; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
