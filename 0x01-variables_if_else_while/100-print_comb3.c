5bca7025096231019c278d4a4d3c1006661d5faa
Omondi Were <franklinegift@gmail.com>
2022-03-14 14:27:28 -0700

    combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints a combination of numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, c;
+
+	for ( x = '0'; x <= '9'; x++)
+	{
+		for (c = '0'; c <= '9'; c++)
+		{
+			if (x < c)
+			{
+				putchar (x);
+				putchar(c);
+
+				if (x != '8' || x == '8' && x != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,19 +6,18 @@
  */
 int main(void)
 {
-	int i = '0';
-
-	while (i <= '9')
+	int i 
+	
+	for ( i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
+
 		if (i != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		++i;
 	}
 	putchar('\n');
 	return (0);
-
 }
