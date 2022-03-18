48e20c5a0444f54f8e96a4e131fd27ba1fb982d2
Omondi Were <franklinegift@gmail.com>
2022-03-18 13:58:44 -0700

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - prints lathest prime factor of given number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x, max, num = 612852475143;
+	double square = sqrt(num);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (num % x == 0)
+			max = num / x;
+	}
+	printf("%d\n, max);
+	return (0);
+}
