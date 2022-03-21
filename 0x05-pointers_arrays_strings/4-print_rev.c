9adff7082ddb9cf725e3177afe549c132e5209c7
Omondi Were <franklinegift@gmail.com>
2022-03-21 13:00:37 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ *
+ * @s: string to be reversed
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int x;
+
+	while (s[x] != '\0')
+		x++;
+	for (x -= 1; x >= 0; x--)
+		_putchar(s[x]);
+	_putchar('\n');
+}
