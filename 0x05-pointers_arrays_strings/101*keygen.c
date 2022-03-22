5d20a105403ac4158a726af81960c24b3b62eca8
Omondi Were <franklinegift@gmail.com>
2022-03-22 14:26:22 -0700

    password gen

---
+++ 0x05-pointers_arrays_strings/101*keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - generates random valid passwords
+ *
+ * Return: 0
+ */
+int main(void)
+
+{
+	char s[52] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char pwd[22];
+	int i, j, password, c;
+
+	srand(time(0));
+
+	for (i = 0; password < 2772; i++)
+	{
+		n = rand() % 10;
+		pwd[i] = s[j];
+		password += pwd[i];
+	}
+	c = 2772 - password;
+	pwd[i] = c;
+	printf("%s\n", pwd);
+	return (0);
+}
