99d94e1c7bb3bea7ddcc926f454a0948bee703c9
Omondi Were <74902740+renegade620@users.noreply.github.com>
2022-03-22 21:46:47 +0300

    Update 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * rev_string - Reverses a string
+  *
+  * @s: string to be reversed
+  *
+  * Return: nothing
+  */
+void rev_string(char *s)
+{
+	int i, c, k;
+	char *a, z;
+
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = z;
+		a--;
+	}
+}
