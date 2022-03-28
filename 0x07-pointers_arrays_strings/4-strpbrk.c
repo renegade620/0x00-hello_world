76e17711bfd7cb9d6a3c2a1630d42bcef29bc40d
Omondi Were <74902740+renegade620@users.noreply.github.com>
2022-03-28 21:44:59 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,52 +1,31 @@
 #include "main.h"
 
 /**
- * strspn - gets length of a prefix substring                                        
- :wq
-
-     *
-     *   * @s: substring                                                                     
-     *
-     *    * @accept: bytes                                                                    
-     *
-     *     *                                                                                   
-     *
-     *      * Return: number of bytes                                                           
-     *
-     *       */                                                                                  
-
-unsigned int _strspn(char *s, char *accept)                                          
-
+ * strspn - gets length of a prefix substring
+ *
+ * @s: substring                                                                       
+ * @accept: bytes                                                                                                                                                
+ *
+ *Return: number of bytes                                                           
+ */                                                                                  
+char *_strpbrk(char *s, char *accept)                                          
 {                                                                                    
+	int i = 0, j;
 
-	        unsigned int i = 0, j, x = 0;                                                
+	while (s[i])
+    {
+        j = 0;
 
-		                                                                                     
-
-		        while (accept[i])                                                            
-
-				        {                                                                            
-
-						                j = 0;                                                               
-
-								                                                                                     
-
-								                while (s[j] != 32)                                                   
-
-											                {                                                                    
-
-														                if (accept[i] == s[j])                                               
-
-																	                        x++;                                                         
-
-																                j++                                                                  
-
-																			                }                                                                    
-
-										                i++;                                                                 
-
-												        }                                                                            
-
-			        return (x);                                                                  
-
-}                      
+        while (accept[j])
+        {
+            if (s[i] == accept[j])
+            {
+                s += i;
+                return (s);
+            }
+            j++;
+        }
+        i++;
+    }
+    return ('\0');
+}
