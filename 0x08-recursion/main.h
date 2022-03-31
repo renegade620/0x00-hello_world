6591ec8d13d67f1dcd4acbee2d57de02709328b1
Omondi Were <74902740+renegade620@users.noreply.github.com>
2022-04-01 01:35:42 +0300

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,5 @@ int is_prime_number(int n);
 int _sqrt(int n, int i);
 int check_prime(int n, int i);
 int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2)
+int wildcmp(char *s1, char *s2);
 int check_palindrome(char *s);
