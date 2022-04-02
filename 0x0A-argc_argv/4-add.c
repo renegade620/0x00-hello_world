3b95638143330d13484f88fc9299a79af94f84cc
Omondi Were <franklinegift@gmail.com>
2022-04-02 02:06:34 -0700

    add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,21 +17,18 @@ int main(int argc, char **argv)
 	char *c;
 
 	for (i = 1; i < argc; i++)
+	{
+		for (x = 0; argv[i][x]; x++)
 		{
-			for (x = 0; argv[i][x]; x++)
+			if (argv[i][x] <= '0' || argv[i][x] >= '9')
 			{
-				c = argv[i][x];
-				
-				if (c[x] <= '0' || c[x] >= '9')
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
-			sum += atoi(argv[x]);
-			c++;
 		}
-		printf("%d\n", sum);
-	
+		sum += atoi(argv[x]);
+		c++;
+	}
+	printf("%d\n", sum);
 	return (0);
 }
