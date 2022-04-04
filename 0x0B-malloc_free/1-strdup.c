603a50c1b11346dbd31e08f5f7e659a0f0354a62
Omondi Were <74902740+renegade620@users.noreply.github.com>
2022-04-04 21:51:49 +0300

    Update 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  * _strdup - returns a pointer to a new string which is a duplicate of the string 
+  * @str: string to duplicate
+  *
+  * Return: a pointer to the duplicated string, NULL if insufficient memory was available
+  */
+char *_strdup(char *str)
+{
+	int a = 0, i = 1;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	s = malloc((sizeof(char) * i) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	while (a < i)
+	{
+		s[a] = str[a];
+		a++;
+	}
+
+	s[a] = '\0';
+	return (s);
+}
