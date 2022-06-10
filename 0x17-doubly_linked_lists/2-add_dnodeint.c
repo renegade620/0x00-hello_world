975eecd2ee83d27842df499668d38e8952d628d9
Omondi Were <74902740+renegade620@users.noreply.github.com>
2022-06-10 04:14:52 +0300

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
-	if (head)
+	if (*head)
 		(*head)->prev = new;
 	*head = new;
 	return (new);
