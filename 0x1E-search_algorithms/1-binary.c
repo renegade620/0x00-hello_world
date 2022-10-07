f283fc50156de7e78524350e569cedcd22a8b661
Omondi Were <74902740+renegade620@users.noreply.github.com>
2022-10-07 15:42:20 +0300

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -26,7 +26,8 @@ int binary_search(int *array, size_t size, int value)
 		for (i = l; i < r; i++)
 			printf("%d, ", array[i]);
 		printf("%d\n", array[i]);
-		i = l + (r - 1) / 2;
+		
+		i = l + (r - l) / 2;
 		if (array[i] == value)
 			return (i);
 		if (array[i] > value)
